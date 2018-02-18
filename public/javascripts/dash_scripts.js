d035c4ff889965a49ce2157b953f143f17b640b6
Hugh Rundle <hugh@hughrundle.net>
2018-02-18 18:35:52 +1100

    🎁 added cancel button when deleting lists

--- public/javascripts/dash_scripts.js
+++ public/javascripts/dash_scripts.js
@@ -13,7 +13,7 @@ function removeList(id){
 			var failText = document.createTextNode('something went wrong');
 			failDiv.appendChild(failText);
 			listing.appendChild(failDiv);
-			console.log('error')
+			console.error('error')
 		}
 	});
 	req.open('GET', `${window.location.protocol}//${window.location.host}/dashboard/removelist/${id}`, true);
@@ -21,27 +21,67 @@ function removeList(id){
 	req.send();
 }
 
+// cancel deletion
+function cancelDeletion(cancel, id, fc){
+	var title = document.querySelector(`a[name=${id}]`);
+	var button = document.getElementById(id);
+	// remove cancel button completely
+	cancel.parentNode.removeChild(cancel);
+	// change confirm button back
+	button.setAttribute('class', 'delList');
+	button.textContent ='delete';
+	// change text back
+	title.removeAttribute('class');
+}
+
 // listen for delete confirmation
 function getDelButton(id){
 	var delButton = document.querySelector('.confirm');
-	delButton.addEventListener('click', function(event){
-		// now check the list and delete the listing once confirmed
-		removeList(id)
-	})
+	var cancelButton = document.querySelector('.undelete');
+
+	function confirm(event) {
+			// now check the list and delete the listing once confirmed
+			removeList(id)
+		}
+
+	function cancel(event) {
+			// we need to remove the confirm eventListener, otherwise the next delete click will be treated as a confirm.
+			delButton.removeEventListener('click', confirm);
+			// cancelButton.removeEventListener('click', cancel);
+			// cancelButton.addEventListener('click', cancel);
+			cancelDeletion(event.target, id, cancel)
+	}
+
+	delButton.addEventListener('click', confirm);
+	cancelButton.addEventListener('click', cancel);
 }
 
-// change delete button to a confirm button
-// TODO create cancel button
+// this loads on page load
+// listen for
 var removeButton = document.getElementsByClassName('delList');
 for (i = 0; i < removeButton.length; i++){
 	removeButton[i].addEventListener('click', function(event){
 		event.preventDefault();
 		var id = event.target.id;
+
+		// change the 'delete' button to a 'confirm' button
 		var button = event.target;
 		var title = document.querySelector(`a[name=${id}]`);
 		title.setAttribute('class', 'strikeout');
 		button.setAttribute('class', 'confirm');
 		button.textContent = 'Confirm you want to delete this list';
+
+		// remove the hint if there is one
+		var hint = document.getElementsByClassName("hint")[0];
+		if (hint) {
+			hint.style.display = "none";
+		}
+
+		// add an undelete button
+		var undelete = document.createElement("button");
+		undelete.setAttribute('class', 'undelete');
+		undelete.textContent = 'Cancel';
+		button.parentElement.appendChild(undelete);
 		// we need to call this as a function otherwise it only runs on page load
 		getDelButton(id)
 	});
