9e9ed9bc5e48b7877efc3df9e4686cb467411dcf
Hugh Rundle <hugh@hughrundle.net>
2018-03-11 12:45:09 +1100

    🎁 unsubscribe from dashboard

--- public/javascripts/dash_scripts.js
+++ public/javascripts/dash_scripts.js
@@ -182,3 +182,21 @@ function publicToggle() {
 	req.setRequestHeader('enctype', 'text/plain')
 	req.send();
 }
+
+// SUBSCRIBE TOGGLE
+// for each list, monitor the state of the subscribe toggle
+const lists = document.getElementsByClassName('subToggle');
+for (list of lists) {
+	list.onchange=subscribeToggle
+}
+
+	// add or remove subscription depending on current state
+	function subscribeToggle() {
+		const id = this.getAttribute('id');
+		const sub = this.checked;
+		// post to server to change setting in DB
+		const req = new XMLHttpRequest();
+		req.open('POST', `${window.location.protocol}//${window.location.host}/lists/subscribe/${id}-${sub}`, true);
+		req.setRequestHeader('enctype', 'text/plain')
+		req.send();
+	}
